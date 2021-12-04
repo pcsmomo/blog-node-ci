f3d333a55c21c7f9d82f41b16a1f86de6dac8682
pcsmomo <dongwoo_333@hanmail.net>
2021-12-05 07:00:16 +1100

    initialization

---
+++ config/dev.js
@@ -0,0 +1,29 @@
+module.exports = {
+  googleClientID:
+    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+  mongoURI:
+    'mongodb://noah:1234@noahcluster-shard-00-00.pvxa3.mongodb.net:27017,noahcluster-shard-00-01.pvxa3.mongodb.net:27017,noahcluster-shard-00-02.pvxa3.mongodb.net:27017/blog_dev?ssl=true&replicaSet=noahcluster-shard-0&authSource=admin&retryWrites=true&w=majority',
+  cookieKey: '123123123111'
+};
+
+// ***********************
+// ** My credentials
+// module.exports = {
+//   googleClientID:
+//     '159106046059-as2d4i8h5fobrk644khe20pt6qpa8jj9.apps.googleusercontent.com',
+//   googleClientSecret: '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',
+//   mongoURI:
+//     'mongodb://noah:1234@noahcluster-shard-00-00.pvxa3.mongodb.net:27017,noahcluster-shard-00-01.pvxa3.mongodb.net:27017,noahcluster-shard-00-02.pvxa3.mongodb.net:27017/blog_dev?ssl=true&replicaSet=noahcluster-shard-0&authSource=admin&retryWrites=true&w=majority',
+//   cookieKey: '123123123111'
+// };
+
+// ***********************
+// ** Stephen's
+// module.exports = {
+//   googleClientID:
+//     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
+//   googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+//   mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+//   cookieKey: '123123123',
+// };
